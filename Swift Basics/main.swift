25d3e013d5a19910ce6ed05468cc2356da260360
Yi Zhao <yz8pd@mail.missouri.edu>
2018-06-11 11:40:03 +0800

    challenge: Swift Basics

--- Swift Basics/main.swift
+++ Swift Basics/main.swift
@@ -7,6 +7,76 @@
 //
 
 import Foundation
+var sample1: uint8 = 0x3A
+var sample2: uint8 = 58
+var heartRate = 85
+var deposits: Double = 135002796
+let acceleration: Float = 9.800
+var mass: Float = 14.6
+var distance:Double = 129.763001
+var lost:Bool = true
+var expensive:Bool = true
+var choice: Int = 2
+let integral: Character = "\u{222B}"
+let greeting: String = "Hello"
+var name:String = "Karen"
 
-print("Hello, World!")
+if sample1==sample2{
+    print("The samples are equal.")
+}else{
+    print("The samples are not equal.")
+}
+
+let heartDescription: String
+switch heartRate{
+case 0..<40 :
+    heartDescription="Heart rate is not normal."
+case 40..<80 :
+    heartDescription="Heart rate is normal."
+default:
+    heartDescription="Heart rate is not normal."
+}
+print("\(heartDescription)")
+
+if deposits>=100000000{
+    print("You are exceedingly wealthy")
+}else{
+    print("Sorry you are so poor.")
+}
+
+var force:Float = acceleration * mass
+print ("force =\(force)")
+
+print("\(distance) is the distance")
+
+if lost==true && expensive==false{
+    print("Here is coupon for 10% off")
+}
+if lost==true && expensive==true{
+    print("I am really sorry! I will get the manager.")
+}
+
+switch choice {
+case 1 :
+    print("You chose 1.")
+case 2 :
+    print("You chose 2.")
+case 3 :
+    print("You chose 3.")
+default:
+    print("You made an unknown choice.")
+}
+
+print("\(integral) is an integal")
+
+
+for i in 5...10{
+    print("i = \(i)")
+}
+ var age = 0
+while age<6{
+    print("age = \(age)")
+    age += 1
+}
+
 
